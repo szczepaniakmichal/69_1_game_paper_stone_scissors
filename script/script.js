c6459adb24edb693ec3b055d382b8c1403640f38
szczepaniakmichal <mssk87@gmail.com>
2019-12-14 00:34:09 +0100

    add function aiChoice, startGame - in progress

--- script/script.js
+++ script/script.js
@@ -21,7 +21,7 @@ const handSelection = (e) => {
     });
     const current = e.currentTarget;
     game.playerHand = current.dataset.option;
-    console.log(game.playerHand);
+    // console.log(game.playerHand);
     current.style.opacity = '1';
     current.style.boxShadow = '0 0 0 5px red';
 };
@@ -30,4 +30,21 @@ hands.forEach(hand => {
    hand.addEventListener('click', handSelection);
 });
 
-console.log(game.playerHand);
+// console.log(game.playerHand);
+
+const aiChoice = () => {
+    const aiChoise = Math.floor(Math.random() * 3);
+    game.aiHand = hands[aiChoise];
+    return game.aiHand.dataset.option;
+};
+
+const startGame = () => {
+    //(game.playerHand ? console.log(game.playerHand) : console.log('wybierz jedna z trzech opcji')) // test ternary operator
+    if (game.playerHand) {
+        game.aiHand = aiChoice();
+    } else {
+        console.log('wybierz jedna z trzech opcji');
+    }
+};
+
+document.querySelector('button.start').addEventListener('click', startGame);;
