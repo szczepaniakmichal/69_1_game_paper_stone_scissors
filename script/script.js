de171dead0a7dcffa6599d6374a7e11ff0698d82
szczepaniakmichal <mssk87@gmail.com>
2019-12-14 22:42:53 +0100

    add function publishResult() - print reselt of game, apdate startGame

--- script/script.js
+++ script/script.js
@@ -48,16 +48,34 @@ const checkResult = (player, ai) => {
     }
 };
 
+const publishResult = (player, ai, result) => {
+  document.querySelector('[data-summary="your-choice"]').textContent = player;
+  document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+  document.querySelector('.numbers span').textContent = ++gameSummary.numbers;
+
+  if (result === 'you win') {
+      document.querySelector('.wins span').textContent = ++gameSummary.wins;
+      document.querySelector('[data-summary="who-win"]').textContent = "Ty";
+      document.querySelector('[data-summary="who-win"]').style.color = 'green';
+  } else if (result === 'you lost') {
+      document.querySelector('.losses span').textContent = ++gameSummary.losses;
+      document.querySelector('[data-summary="who-win"]').textContent = "Komputer";
+      document.querySelector('[data-summary="who-win"]').style.color = 'red';
+  } else {
+      document.querySelector('.draws span').textContent = ++gameSummary.draws;
+      document.querySelector('[data-summary="who-win"]').textContent = "Remis";
+      document.querySelector('[data-summary="who-win"]').style.color = 'orange';
+  }
+};
+
 const startGame = () => {
     //(game.playerHand ? console.log(game.playerHand) : console.log('wybierz jedna z trzech opcji')) // test ternary operator
     if (!game.playerHand) {
         return console.log('wybierz jedna z trzech opcji');
     }
     game.aiHand = aiChoice();
-    console.log(game.playerHand);
-    console.log(game.aiHand);
     const gameResult = checkResult(game.playerHand, game.aiHand);
-    console.log(gameResult);
+    publishResult(game.playerHand, game.aiHand, gameResult);
 };
 
 document.querySelector('button.start').addEventListener('click', startGame);
